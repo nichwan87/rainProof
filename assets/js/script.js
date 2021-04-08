5e29e562cb07bc5e36fd3839fda0b7b48c450883
Will Locket <will.locket@fastmail.com>
2021-04-08 15:57:16 +1000

    events api on load, date picker with calender, start of google map include event results page scroller, css, html and javascript updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,221 @@
+var ticketAPI = 'EMZOAA3KlATktn9bwYV8aKh7yFnEm92G';
+var googleKey = 'AIzaSyBGnpmmzWpz-SVHgMnChfl2Vw3_jkf6cCk';
+var page = 0;
+var map;
 
-//Sets top of page current date and time
 
+// add Moment.js for current date 
+// placeholder when page is loaded
+$(document).ready(function() {
+    
+    $("#date").val(moment().format('YYYY-MM-DD')) 
 
-const date = new Date();
-
-const today = date.toDateString();
-
-timer();
-
-function timer(){
-var currentTime = new Date()
-var hours = currentTime.getHours()
-var minutes = currentTime.getMinutes()
-var sec = currentTime.getSeconds()
-if (minutes < 10){
-    minutes = "0" + minutes
-}
-if (sec < 10){
-    sec = "0" + sec
-}
-var t_str = hours + ":" + minutes + ":" + sec + " ";
-if(hours > 11){
-    t_str += "PM";
-} else {
-   t_str += "AM";
-}
-document.getElementById('now').innerHTML = today + ' @ ' + t_str;
- setTimeout(timer,1000);
-}
-
-const findBtn = document.querySelector("#findBtn");
-const listEl = document.querySelector("#results");
-
-findBtn.addEventListener("click" , ()=> {
-    listEl.style.display = "block";
 })
 
-//https://developers.google.com/maps/
 
 
-// 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+/* locations dropdown opens on click */
+var dropdown = document.querySelector('.dropdown');
+dropdown.addEventListener('click', function(event) {
+  event.stopPropagation();
+  dropdown.classList.toggle('is-active');
+  
+});
+
+
+
+
+
+// load events function depending on page
+  function getEvents(page) {
+//show hide panels 
+  $('#events-panel').show();
+  $('#attraction-panel').hide();
+
+
+  if (page < 0) {
+    page = 0;
+    return;
+  }
+  if (page > 0) {
+    if (page > getEvents.json.page.totalPages-1) {
+      page=0;
+    }
+  }
+
+
+
+// main Events load
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=EMZOAA3KlATktn9bwYV8aKh7yFnEm92G&page="+page,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+          getEvents.json = json;
+  			  showEvents(json);
+              /* console.log(json); */
+  		   },
+
+    error: function(xhr, status, err) {
+  			  console.log(err);
+  		   }
+  });
+}
+
+
+
+
+function showEvents(json) {
+    var items = $('#events .list-group-item');
+    items.hide();
+
+    var events = json._embedded.events;
+    var item = items.first();
+
+    for (var i=0;i<events.length;i++) {
+      item.children('.list-group-item-heading').text(events[i].name);
+      item.children('.list-group-item-text').text(events[i].dates.start.localDate);
+      try {
+        item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
+      } 
+      catch (err) {
+        console.log(err);
+      }
+
+      item.show();
+      item.off("click");
+      item.click(events[i], function(eventObject) {
+        console.log(eventObject.data);
+        try {
+          getAttraction(eventObject.data._embedded.attractions[0].id);
+        } 
+        catch (err) {
+        console.log(err);
+        }
+      });
+      item=item.next();
+    }
+  }
+
+
+
+
+// arrows on click change page up or down
+$('#prev').click(function() {
+    getEvents(--page);
+    if (page = 1) {
+        return;
+      }
+  
+  });
+  
+  $('#next').click(function() {
+    getEvents(++page);
+  });
+  
+
+
+
+
+
+// loads a single event if clicked and show/hides panels
+function getAttraction(id) {
+    $.ajax({
+      type:"GET",
+      url:"https://app.ticketmaster.com/discovery/v2/attractions/"+id+".json?apikey=EMZOAA3KlATktn9bwYV8aKh7yFnEm92G",
+      async:true,
+      dataType: "json",
+      success: function(json) {
+            showAttraction(json);
+               },
+      error: function(xhr, status, err) {
+                  console.log(err);
+               }
+    });
+  }
+  // shows the attraction panel for a clicked event
+  function showAttraction(json) {
+    $('#events-panel').hide();
+    $('#attraction-panel').show();
+    
+    $('#backBtn').click(function() {
+      getEvents(page);
+    });
+    
+    $('#attraction .list-group-item-heading').first().text(json.name);
+    $('#attraction img').first().attr('src',json.images[0].url);
+    $('#classification').text(json.classifications[0].segment.name + " - " + json.classifications[0].genre.name + " - " + json.classifications[0].subGenre.name);
+    $('#ticketLink').text(json.url);
+   /*  $('#infoText').text(json._embedded.events[0].address.line1); */
+   
+  }
+  
+  
+  // initial call to load events
+  getEvents(page);
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * use google maps api built-in mechanism to attach dom events
+ */
+google.maps.event.addDomListener(window, "load", function () {
+
+  /*
+   * create map
+   */
+  var map = new google.maps.Map(document.getElementById("map_div"), {
+    center: new google.maps.LatLng(33.808678, -117.918921),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  /*
+   * create infowindow (which will be used by markers)
+   */
+  var infoWindow = new google.maps.InfoWindow();
+
+  /*
+   * marker creater function (acts as a closure for html parameter)
+   */
+  function createMarker(options, html) {
+    var marker = new google.maps.Marker(options);
+    if (html) {
+      google.maps.event.addListener(marker, "click", function () {
+        infoWindow.setContent(html);
+        infoWindow.open(options.map, this);
+      });
+    }
+    return marker;
+  }
+
+  /*
+   * add markers to map
+   */
+  var marker0 = createMarker({
+    position: new google.maps.LatLng(33.808678, -117.918921),
+    map: map,
+    icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
+  }, "<h1>Marker 0</h1><p>This is the home marker.</p>");
+
+  var marker1 = createMarker({
+    position: new google.maps.LatLng(33.818038, -117.928492),
+    map: map
+  }, "<h1>Marker 1</h1><p>This is marker 1</p>");
+
+  var marker2 = createMarker({
+    position: new google.maps.LatLng(33.803333, -117.915278),
+    map: map
+  }, "<h1>Marker 2</h1><p>This is marker 2</p>");
+});
+
